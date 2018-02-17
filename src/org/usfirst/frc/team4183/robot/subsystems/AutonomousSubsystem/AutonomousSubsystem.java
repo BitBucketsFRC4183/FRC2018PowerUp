ac0f19b3a432f3599cb638b27c74c5bfb03a39e2
Christopher McCormick <cmccormick2@gregoryschool.org>
2018-02-17 09:16:28 -0700

    Starts of Autonomous Initial Position

--- src/org/usfirst/frc/team4183/robot/subsystems/AutonomousSubsystem/AutonomousSubsystem.java
+++ src/org/usfirst/frc/team4183/robot/subsystems/AutonomousSubsystem/AutonomousSubsystem.java
@@ -1,8 +1,12 @@
 package org.usfirst.frc.team4183.robot.subsystems.AutonomousSubsystem;
 
 import org.usfirst.frc.team4183.robot.subsystems.BitBucketsSubsystem;
+import org.usfirst.frc.team4183.utils.Positions;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  *
@@ -11,7 +15,42 @@ public class AutonomousSubsystem extends BitBucketsSubsystem {
 
 	// Nothing in here now...it's pretty much a dummy,
 	// just to make the state machine work.
+	
+	private static SendableChooser<Positions.StartingPosition> startingPosition;
 
+	private static Positions.GenericPositions scalePositon;
+	private static Positions.GenericPositions switchPosition;
+	
+	
+	public AutonomousSubsystem()
+	{
+		startingPosition = new SendableChooser<Positions.StartingPosition>();
+		startingPosition.addDefault("Center", Positions.StartingPosition.CENTER);
+		startingPosition.addObject("LEFT", Positions.StartingPosition.LEFT);
+		startingPosition.addObject("Right", Positions.StartingPosition.RIGHT);
+		
+		SmartDashboard.putData(startingPosition);
+		
+	}
+	
+	public void convertGameData()
+	{
+		String gameData;
+		gameData = DriverStation.getInstance().getGameSpecificMessage();
+		
+		if (gameData.length() > 0)
+		{
+			if (gameData.charAt(0) == 'L')
+			{
+				
+			}
+			else if (gameData.charAt(0) == 'R') 
+			{
+				
+			}
+		}
+	}
+	
     public void initDefaultCommand() {
         setDefaultCommand(new Idle());
     }
