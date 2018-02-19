db9eadb5543a0f103b2ffc8c312e0acafeec42ec
RocketRedNeck <the.RocketRedNeck@gmail.com>
2018-02-19 08:20:53 -0700

    Insert 180 turn assist

--- src/org/usfirst/frc/team4183/robot/subsystems/DriveSubsystem/DriverControl.java
+++ src/org/usfirst/frc/team4183/robot/subsystems/DriveSubsystem/DriverControl.java
@@ -49,15 +49,10 @@ public class DriverControl extends Command
     	{
     		return CommandUtils.stateChange(this, new AlignLock());
     	}
-    	else if (Robot.oi.btnMoveByTest.get())
+    	else if (Robot.oi.btn180.get())
     	{
-    		return CommandUtils.stateChange(this, new MoveBy(36.0,5.0));
-    	}
-    	
-    	else if (Robot.oi.btnMoveByTest.get()) 
-        { 
-          return CommandUtils.stateChange(this, new MoveBy(3600.0,5.0)); 
-        } 
+    		return CommandUtils.stateChange(this, new TurnBy(180.0,5.0));
+    	} 
     	
         return false;
     }
