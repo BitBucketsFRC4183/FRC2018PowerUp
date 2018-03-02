289a2254fb9a7176252ed1fcaa8345111fc7b08c
Joseph Acevedo <jacevedo@bitbuckets.org>
2018-03-02 10:31:38 -0700

    ElevatorIdle

---
+++ src/org/usfirst/frc/team4183/robot/subsystems/ElevatorSubsystem/Idle.java
@@ -0,0 +1,64 @@
+package org.usfirst.frc.team4183.robot.subsystems.ElevatorSubsystem;
+
+import org.usfirst.frc.team4183.robot.Robot;
+import org.usfirst.frc.team4183.utils.CommandUtils;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class Idle extends Command{
+
+	public Idle()
+	{
+		requires(Robot.elevatorSubsystem);
+		setRunWhenDisabled(true);
+	}
+	
+	protected void initialize()
+	{
+		Robot.elevatorSubsystem.disable();
+		//Robot.elevatorSubsystem.holdEncodPos(false);
+		//Robot.elevatorSubsystem.holdEncodPos(true);
+	}
+	
+	public void execute()
+	{
+		
+	}
+	
+	protected boolean isFinished()
+	{ /*
+		if (Robot.intakeSubsystem.getLastCurrent())
+		{
+			return CommandUtils.stateChange(this, new Loaded());
+		}
+		
+		if (!Robot.intakeSubsystem.getLastCurrent())
+		{
+			return CommandUtils.stateChange(this, new Empty());
+		}
+		if (timeSinceInitialized() > Robot.elevatorSubsystem.timeUntilBrakeSec)
+		{
+			Robot.elevatorSubsystem.engageBrake();
+			return CommandUtils.stateChange(this, new Brake());
+		}
+		*/
+		/// Temporary code for testing linkage
+		/// TODO: magic 0.06
+		if (Math.abs(Robot.oi.leftRampAxis.get()) > .06)		/// TODO: Need comment here explaining that sharing joytick!
+		{														/// TODO: May want two instances with different names and put comment at definition
+			return CommandUtils.stateChange(this, new Reposition());
+		}
+		return false;
+	}
+
+	protected void end()
+	{
+		
+	}
+	
+	protected void interrupted()
+	{
+		end();
+	}
+	
+}
